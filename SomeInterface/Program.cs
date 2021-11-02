766f325e30cec797e13cd9d421b1d3800dcbf42d
Супрун Сергей <4everyongg@mail.ru>
2021-11-02 21:46:52 +0400

    Add project files.

---
+++ SomeInterface/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace SomeInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public interface ISomeInterface
+    {
+        public int X { get; }
+        public string Foo(string arg1);
+        public void Bar(int number);
+    }
+
+    class A : ISomeInterface
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public string Foo(string arg1)
+        {
+            return arg1;
+        }
+
+        public void Bar(int nubmer)
+        {
+
+        }
+
+        public A Process(A a)
+        {
+            return a;
+        }
+    }
+
+    class B : ISomeInterface
+    {
+        public int X { get; }
+
+        public string Foo(string arg1)
+        {
+            return arg1;
+        }
+
+        public void Bar(int nubmer)
+        {
+
+        }
+
+        private void DoSomething(double a)
+        {
+
+        }
+    }
+
+    class Consumer
+    {
+        public void UseSomeInterface(ISomeInterface someInterface) { }
+    }
+}
